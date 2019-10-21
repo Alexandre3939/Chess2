70de91ef123f59e1d9fe0e9a975390254205c23c
Alexandre3939 <apdo3939@gmail.com>
2019-10-21 16:56:04 -0300

    First Class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Position p = new Position(2,0);
+		System.out.println("A posição é: " + p.toString());
+		
+	}
+
+}
